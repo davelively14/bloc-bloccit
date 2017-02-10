81e04f8f935102fb67892ad0125f79152a90445f
Dave Lively <davelively14@gmail.com>
2017-02-10 12:38:28 -0500

    Assignment 22 completed via TDD and CRUD

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -8,7 +8,51 @@ class SponsoredPostsController < ApplicationController
     @sponsored_post = SponsoredPost.new
   end
 
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @sponsored_post = SponsoredPost.new
+    @sponsored_post.title = params[:sponsored_post][:title]
+    @sponsored_post.body = params[:sponsored_post][:body]
+    @sponsored_post.price = params[:sponsored_post][:price]
+    @sponsored_post.topic = @topic
+
+    if @sponsored_post.save
+      flash[:notice] = "Post '#{@sponsored_post.title[0..10]}...' was saved."
+      redirect_to [@topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error saving the Sponsored Post. Please try again."
+      render :new
+    end
+  end
+
   def edit
     @sponsored_post = SponsoredPost.find(params[:id])
+  end
+
+  def update
+    @sponsored_post = SponsoredPost.find(params[:id])
+    @sponsored_post.title = params[:sponsored_post][:title]
+    @sponsored_post.body = params[:sponsored_post][:body]
+    @sponsored_post.price = params[:sponsored_post][:price]
+
+    if @sponsored_post.save
+      flash[:notice] = "Sponsored Post was updated."
+      redirect_to [@sponsored_post.topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error saving the Sponsored Post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @sponsored_post = SponsoredPost.find(params[:id])
+
+    if @sponsored_post.destroy
+      flash[:notice] = "\"#{@sponsored_post.title[0..20]}\" was deleted successfully."
+      redirect_to @sponsored_post.topic
+    else
+      flash.now[:alert] = "There was an error deleting the Sponsored Post."
+      render :show
+    end
   end
 end
--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -1,26 +1,117 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPostsController, type: :controller do
+  let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:my_sp) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 25) }
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, topic_id: my_topic.id, id: my_sp.id
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, topic_id: my_topic.id, id: my_sp.id
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_sp to @sponsored_post" do
+      get :show, topic_id: my_topic.id, id: my_sp.id
+      expect(assigns(:sponsored_post)).to eq(my_sp)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      get :new, topic_id: my_topic.id
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new, topic_id: my_topic.id
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @post" do
+      get :new, topic_id: my_topic.id
+      expect(assigns(:sponsored_post)).not_to be_nil
+    end
+  end
+
+  describe "POST #create" do
+    it "increments the number of SponsoredPost by 1" do
+      expect{post :create, topic_id: my_topic.id, sponsored_post: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 25}}.to change(SponsoredPost, :count).by(1)
+    end
+
+    it "assigns the new post to @post" do
+      post :create, topic_id: my_topic.id, sponsored_post: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 25}
+      expect(assigns(:sponsored_post)).to eq SponsoredPost.last
+    end
+
+    it "redirects to the new post" do
+      post :create, topic_id: my_topic.id, sponsored_post: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 25}
+      expect(response).to redirect_to [my_topic, SponsoredPost.last]
     end
   end
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      get :edit, topic_id: my_topic.id, id: my_sp.id
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, topic_id: my_topic.id, id: my_sp.id
+      expect(response).to render_template(:edit)
+    end
+
+    it "assigns post to be udpated to @sponsored_post" do
+      get :edit, topic_id: my_topic.id, id: my_sp.id
+
+      sp_instance = assigns(:sponsored_post)
+
+      expect(sp_instance.id).to eq my_sp.id
+      expect(sp_instance.title).to eq my_sp.title
+      expect(sp_instance.body).to eq my_sp.body
+      expect(sp_instance.price).to eq my_sp.price
     end
   end
 
+  describe "PUT #update" do
+    it "updates sponsored_post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_price = 30
+
+      put :update, topic_id: my_topic.id, id: my_sp.id, sponsored_post: {title: new_title, body: new_body, price: new_price}
+
+      updated_sp = assigns(:sponsored_post)
+      expect(updated_sp.id).to eq my_sp.id
+      expect(updated_sp.title).to eq new_title
+      expect(updated_sp.body).to eq new_body
+      expect(updated_sp.price).to eq new_price
+    end
+
+    it "redirects to the updated sponsored post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_price = 30
+
+      put :update, topic_id: my_topic.id, id: my_sp.id, sponsored_post: {title: new_title, body: new_body, price: new_price}
+      expect(response).to redirect_to [my_topic, my_sp]
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes the sponsored post" do
+      delete :destroy, topic_id: my_topic.id, id: my_sp.id
+      count = SponsoredPost.where({id: my_sp.id}).size
+      expect(count).to eq(0)
+    end
+
+    it "redirects to topic show" do
+      delete :destroy, topic_id: my_topic.id, id: my_sp.id
+      expect(response).to redirect_to my_topic
+    end
+  end
 end
