2041d3c4e80d46cf6d1a6ead8738e341aa1e9713
Dave Lively <davelively14@gmail.com>
2017-02-14 12:17:04 -0500

    Checkpoint 29 complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  # Nest comments under posts. The only: [] prevents us from creating any
+  # additional post routes, as we've nested those under topics.
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
