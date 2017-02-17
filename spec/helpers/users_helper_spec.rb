dc67cb9c869d855420a00471fc1b898a0489693b
Dave Lively <davelively14@gmail.com>
2017-02-17 18:09:23 -0500

    Added favorites factory and tests for UsersHelper

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,5 +11,38 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, type: :helper do
-  
+  let(:user) { create(:user) }
+
+  describe "#has_posts?" do
+    it "returns false if user has no posts" do
+      expect(helper.has_posts?(user)).to be_falsey
+    end
+
+    it "returns true if user made any posts" do
+      create(:post, user: user)
+      expect(helper.has_posts?(user)).to be_truthy
+    end
+  end
+
+  describe "#has_comments?" do
+    it "returns false if no comments made" do
+      expect(helper.has_comments?(user)).to be_falsey
+    end
+
+    it "returns true if comment has been made" do
+      create(:comment, user: user)
+      expect(helper.has_comments?(user)).to be_truthy
+    end
+  end
+
+  describe "#has_favorites?" do
+    it "returns false if no favorites made" do
+      expect(helper.has_favorites?(user)).to be_falsey
+    end
+
+    it "returns true if favorites have been made" do
+      create(:favorite, user: user)
+      expect(helper.has_favorites?(user)).to be_truthy
+    end
+  end
 end
